8837711386e99a372d556b16207f5972eac1ca30
cocococococooo <chengyuting852@gmail.com>
2026-06-03 23:09:52 +0800

    fix: change OCR text area height to 250px

--- mobile/app/note/[id].tsx
+++ mobile/app/note/[id].tsx
@@ -1058,7 +1058,7 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     color: "#52525B",
     paddingVertical: 0,
-    height: 230,
+    height: 250,
   },
   ncOcrRetry: {
     flexDirection: "row",
